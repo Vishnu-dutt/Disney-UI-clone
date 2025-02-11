51a644fb8b13bf0b8de557bca355b9e2c7d91664
Vishnu-dutt <ankitkumarrai769631@gmail.com>
2025-02-12 00:34:52 +0530

    login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,19 @@ import styled from "styled-components";
 const Login = (props) => {
   return (
     <Container>
-      <Content>Content</Content>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 26/03/2024, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt=""/>
+        </CTA>
+        <BgImage />
+      </Content>
     </Container>
   );
 };
@@ -29,6 +41,77 @@ const Content = styled.div`
   flex-direction: column;
   padding: 80px 40px;
   height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+  width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const CTALogoTwo = styled.img`
+  margin-bottom: 20px;
+  max-width: 600px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: white;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  padding: 16.5px 0;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5em;
+  letter-spacing: 1.5px;
 `;
 
 export default Login;
