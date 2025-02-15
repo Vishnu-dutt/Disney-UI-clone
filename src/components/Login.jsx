9233813d6b865f91bcf5990818c46f4ed6215695
Vishnu-dutt <ankitkumarrai769631@gmail.com>
2025-02-15 12:42:38 +0530

    added login auth to the GET ALL HERE Button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,79 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { auth, provider } from "../firebase/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setUserLoginDetails,
+  setSignOutState,
+} from "../features/user/userSlice";
 
-const Login = (props) => {
+const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        navigate("/home");
+      }
+    });
+  }, [userName]);
+
+  const handleAuth = () => {
+    if (!userName) {
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          dispatch(
+            setUserLoginDetails({
+              displayName: result.user.displayName,
+              email: result.user.email,
+              photoURL: result.user.photoURL,
+            })
+          );
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (userName) {
+      auth
+        .signOut()
+        .then(() => {
+          dispatch(setSignOutState());
+          navigate("/");
+        })
+        .catch((error) => alert(error.message));
+    }
+  };
+
+  const setUser = (user) => {
+    dispatch(
+      setUserLoginDetails({
+        displayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+      })
+    );
+  };
+
   return (
     <Container>
       <Content>
         <CTA>
           <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
-          <SignUp>GET ALL THERE</SignUp>
+          <SignUp onClick={handleAuth}>GET ALL THERE</SignUp>
           <Description>
             Get Premier Access to Raya and the Last Dragon for an additional fee
             with a Disney+ subscription. As of 26/03/2025, the price of Disney+
             and The Disney Bundle will increase by $1.
           </Description>
-          <CTALogoTwo src="/images/cta-logo-two.png" alt=""/>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
         </CTA>
         <BgImage />
       </Content>
@@ -100,7 +160,7 @@ const SignUp = styled.a`
   font-size: 18px;
   border: 1px solid transparent;
   border-radius: 4px;
-
+  cursor: pointer;
   &:hover {
     background-color: #0483ee;
   }
